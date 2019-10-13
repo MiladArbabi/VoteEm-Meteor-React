5ef703fd8ca2684b6a7d342b31cddc814212eae1
Milad. A <46928343+MiladArbabi@users.noreply.github.com>
2019-10-14 00:26:32 +0200

    wa-02 adding mongoDB and using meteor devtools

--- imports/client/App.js
+++ imports/client/App.js
@@ -1,31 +1,21 @@
 import React, { Component } from 'react';
+import Items from '../api/items';
+import { createContainer } from 'meteor/react-meteor-data';
 
-export default class App extends Component {
-  constructor(){
-    super();
-    this.state ={
-      count: 0
-    }
-  }
-
-  headingClick() {
-    this.setState({count: this.state.count + 1});
-  }
+class App extends Component {
 
   render() {
     return (
-        <header onClick={this.headingClick.bind(this)}>
-          
-          <Heading count={this.state.count}/>
+        <header>
+          <h1>Level Up Voting</h1>
         </header>
     );
   }
 }
 
-class Heading extends Component {
-  render() {
-    return(
-      <h1>{this.props.count}</h1>
-    )
+export default createContainer(() => {
+  return {
+    items: Items.find({}).fetch()
+    //basic find function and call fetch to get array and pass to items
   }
-}
+}, App);
--- server/main.js
+++ server/main.js
@@ -1,5 +1,19 @@
 import { Meteor } from 'meteor/meteor';
 
+import Items from '../imports/api/items';
+
+const Tests = new Mongo.Collection('tests');
+
 Meteor.startup(() => {
+  Items.insert({
+    itemOne: {
+      text: 'Hi',
+      value: 0
+    },
+    itemTwo: {
+      text: 'Hello',
+      value: 0
+    },
+  });
   // code to run on server at startup
 });
